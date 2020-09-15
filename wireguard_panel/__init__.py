002e8454d21c0d487e0467d9187d9dd787791eff
Galen Guyer <galen@galenguyer.com>
2020-09-15 14:59:00 -0400

    login return to next param if exists

--- wireguard_panel/__init__.py
+++ wireguard_panel/__init__.py
@@ -159,7 +159,7 @@ def login_post():
 
     # if the above check passes, then we know the user has the right credentials
     login_user(user, remember=remember)
-    return redirect('/')    return redirect(request.args.get('next') if request.args.get('next') else '/')
+    return redirect(request.args.get('next') if request.args.get('next') else '/')
 
 @APP.route('/logout')
 @login_required
